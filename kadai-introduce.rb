18300bb8b654b6f2a23157e7e51c60bc04ba9f08
taki <chainmaker85@gmail.com>
2021-08-03 14:53:44 +0000

    modify

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,4 @@
 family_name = '瀧嶋'
 first_name = '洋祐'
 age = 36
-str1 = 'です。'  
-str2 = '歳です。'
-puts family_name + first_name + str1 + age.to_s + str2
+puts family_name + first_name + 'です。' + age.to_s + '歳です。'
